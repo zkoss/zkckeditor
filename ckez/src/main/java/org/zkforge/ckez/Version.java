f845d4237db7d338ff4faf4f8ab141f17beca26b
Rudy Huang <rudyhuang@potix.com>
2020-04-29 11:49:54 +0800

    upgrade to version 4.14.0.0

--- ckez/src/main/java/org/zkforge/ckez/Version.java
+++ ckez/src/main/java/org/zkforge/ckez/Version.java
@@ -25,5 +25,5 @@ package org.zkforge.ckez;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "4.11.1.1";
+	public static final String UID = "4.14.0.0";
 }
