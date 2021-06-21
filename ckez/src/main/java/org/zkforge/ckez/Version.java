eb4ca9c756353bf2940b798a40c3a657191dc205
Rudy Huang <rudyhuang@potix.com>
2021-06-21 11:12:34 +0800

    upgrade to version 4.16.1.0

--- ckez/src/main/java/org/zkforge/ckez/Version.java
+++ ckez/src/main/java/org/zkforge/ckez/Version.java
@@ -25,5 +25,5 @@ package org.zkforge.ckez;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "4.14.0.2";
+	public static final String UID = "4.16.1.0";
 }
