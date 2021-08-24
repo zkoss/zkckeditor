fc3de3437332f90a3ca3cb85b07008b4c01622e8
Katherine <b952426@gmail.com>
2021-08-24 10:46:32 +0800

    upgrade version to 4.16.1.2

--- ckez/src/main/java/org/zkforge/ckez/Version.java
+++ ckez/src/main/java/org/zkforge/ckez/Version.java
@@ -25,5 +25,5 @@ package org.zkforge.ckez;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "4.16.1.1";
+	public static final String UID = "4.16.1.2";
 }
