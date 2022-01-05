b5aa01db50f76957f44d12f9d7c3a3e79121f206
Katherine <b952426@gmail.com>
2022-01-05 14:53:41 +0800

    ZKCK-73: update to 4.17 for security fixes

--- ckez/src/main/java/org/zkforge/ckez/Version.java
+++ ckez/src/main/java/org/zkforge/ckez/Version.java
@@ -25,5 +25,5 @@ package org.zkforge.ckez;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "4.16.2.0";
+	public static final String UID = "4.17.0.0";
 }
