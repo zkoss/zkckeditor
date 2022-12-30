fa620daca48f6a70f58675e542f5b11d614d4980
learningstud <gordonhsu@potix.com>
2022-12-30 14:36:22 +0800

    Fix the testcafe warning: "pass the selector property without 'await'"
    Full error message:
    You passed a DOM snapshot property to the assertion's 't.expect()' method. The property value is assigned when the snapshot is resolved and this value is no longer updated. To ensure that the assertion verifies an
      up-to-date value, pass the selector property without 'await'.
    
     See the following links for more detail:
     https://github.com/DevExpress/testcafe/issues/5389
     https://testcafe.io/documentation/402670/reference/test-api/domnodestate

--- test/addon/ckeztest/test/B-ZKCK-51.ts
+++ test/addon/ckeztest/test/B-ZKCK-51.ts
@@ -1,4 +1,5 @@
 import { Selector } from 'testcafe';
+import * as assert from 'assert';
 
 fixture('ZKCK-51')
 	.page('http://localhost:8080/ckeztest/test2/B-ZKCK-51.zul');
@@ -7,8 +8,10 @@ test('test', async t => {
 	await t
 		.click('.z-groupbox-title');
 
-	const ckeditor = Selector('.z-ckeditor'),
-		groupbox = Selector('.z-groupbox-content');
-
-	await t.expect(groupbox.clientHeight).gte(await ckeditor.clientHeight);
+	// https://testcafe.io/documentation/402670/reference/test-api/domnodestate
+	const ckeditor = await Selector('.z-ckeditor')(),
+		groupbox = await Selector('.z-groupbox-content')();
+	assert.ok(ckeditor.clientHeight, '`clientHeight` of ".z-ckeditor" is undefined');
+	assert.ok(groupbox.clientHeight, '`clientHeight` of ".z-groupbox-content" is undefined');
+	assert.ok(groupbox.clientHeight >= ckeditor.clientHeight);
 });
